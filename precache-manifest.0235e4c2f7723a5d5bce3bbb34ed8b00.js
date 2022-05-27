73900291b38cc63ded8ea737ab03b1950013d35f
Visanth1135 <k.visanth1996@gmail.com>
2022-05-27 19:54:58 +0400

    Updates

---
+++ precache-manifest.0235e4c2f7723a5d5bce3bbb34ed8b00.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ec046d9f89a8d2dd5ae7b6da6689c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf17cecbe6dc233aaa08",
+    "url": "/static/css/2.41005f29.chunk.css"
+  },
+  {
+    "revision": "d1663e0f87bcfbd2127b",
+    "url": "/static/css/main.cb77ac9b.chunk.css"
+  },
+  {
+    "revision": "bf17cecbe6dc233aaa08",
+    "url": "/static/js/2.53ae3f0a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.53ae3f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1663e0f87bcfbd2127b",
+    "url": "/static/js/main.5fbde47f.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  },
+  {
+    "revision": "7d1fe24b3072b92a2bd11f8a5d569f9b",
+    "url": "/static/media/me-about.7d1fe24b.jpg"
+  },
+  {
+    "revision": "0a8cf10ed941810c0051a81e9a1572eb",
+    "url": "/static/media/me.0a8cf10e.png"
+  },
+  {
+    "revision": "d1e8fb803162800c55cbc18b57899262",
+    "url": "/static/media/portfolio1.d1e8fb80.jpg"
+  },
+  {
+    "revision": "13db5e2202aa39b783fded70d877b662",
+    "url": "/static/media/portfolio2.13db5e22.jpg"
+  },
+  {
+    "revision": "ce79413f3dfb4b9ab668d3236a4a04d1",
+    "url": "/static/media/portfolio3.ce79413f.jpg"
+  },
+  {
+    "revision": "04dbe3ab7df1162d6c760d973af4ad6a",
+    "url": "/static/media/portfolio4.04dbe3ab.jpg"
+  },
+  {
+    "revision": "c6e34db9e36b84eb604b3d8897e2b5c1",
+    "url": "/static/media/portfolio5.c6e34db9.png"
+  },
+  {
+    "revision": "32a902840cca71090c80890561f4be7f",
+    "url": "/static/media/portfolio6.32a90284.jpg"
+  },
+  {
+    "revision": "35ae84ec85745f85a16992951a4fc181",
+    "url": "/static/media/visanth-React-resume.35ae84ec.pdf"
+  }
+]);
